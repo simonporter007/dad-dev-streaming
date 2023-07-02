f034e19f1216ed213db84e7310256dddee208287
simonporter007 <simonporter007@users.noreply.github.com>
2023-07-02 22:03:30 +0100

    add overlay app and twitch pomo timer to dad bot

--- apps/dad-bot/src/utils/axiosInstance.ts
+++ apps/dad-bot/src/utils/axiosInstance.ts
@@ -26,6 +26,12 @@ axiosClient.interceptors.response.use(
     ) {
       tokens.twitchAccessToken = res.data.access_token;
     }
+    if (
+      res.config.url === 'https://id.twitch.tv/oauth2/token' &&
+      res.data.access_token
+    ) {
+      tokens.twitchAccessToken = res.data.access_token;
+    }
     return res;
   },
   async (err) => {
@@ -48,6 +54,28 @@ axiosClient.interceptors.response.use(
           return axiosClient(originalConfig);
         } catch (_error) {
           tokens.spotifyAccessToken = '';
+          return Promise.reject(_error);
+        }
+      }
+      return Promise.reject(err);
+    } else if (
+      originalConfig.url !== '/auth/twitch/refresh' &&
+      !originalConfig.url.includes('spotify') &&
+      err.response
+    ) {
+      // token expired
+      if (
+        err.response.status === 401 &&
+        !originalConfig._retry &&
+        err.response.message !== 'No token provided'
+      ) {
+        originalConfig._retry = true;
+        try {
+          const resp = await axiosClient.post('/auth/twitch/refresh');
+          tokens.twitchAccessToken = resp?.data?.accessToken;
+          return axiosClient(originalConfig);
+        } catch (_error) {
+          tokens.twitchAccessToken = '';
           return Promise.reject(_error);
         }
       }
