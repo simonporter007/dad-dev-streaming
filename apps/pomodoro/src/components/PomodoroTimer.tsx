b0ca70770bdb0676eb4f6fcd2f96af37358986cc
simonporter007 <simonporter007@users.noreply.github.com>
2023-07-04 23:51:14 +0100

    add ws connection and fix refresh issue

--- apps/pomodoro/src/components/PomodoroTimer.tsx
+++ apps/pomodoro/src/components/PomodoroTimer.tsx
@@ -83,8 +83,6 @@ export function PomodoroTimer() {
     return () => clearInterval(interval);
   }, [timerStarted, onBreak]);
 
-  console.log({ messageHistory });
-
   return (
     <a href='#' onClick={handleTimerClick}>
       {onBreak ? (
