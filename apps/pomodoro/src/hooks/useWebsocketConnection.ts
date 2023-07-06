a1846ccaf52a02268f29b23bc106bc8e7f3694e7
simonporter007 <simonporter007@users.noreply.github.com>
2023-07-06 12:46:22 +0100

    add reconnection attempts for websocket

--- apps/pomodoro/src/hooks/useWebsocketConnection.ts
+++ apps/pomodoro/src/hooks/useWebsocketConnection.ts
@@ -3,7 +3,11 @@ import useWebSocket from 'react-use-websocket';
 
 export function useWebsocketConnection() {
   const [socketUrl] = useState('wss://daddevbot-websocket.simonporter.co.uk');
-  const { lastMessage } = useWebSocket(socketUrl);
+  const { lastMessage } = useWebSocket(socketUrl, {
+    shouldReconnect: () => true,
+    reconnectAttempts: 20,
+    reconnectInterval: 3000,
+  });
 
   return lastMessage;
 }
